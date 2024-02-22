fd995cca2f74c452a2da69f811a24a9701698b0d
Draggner <devdraggnerstudio@gmail.com>
2024-02-22 14:33:38 -0300

    NLW Expert: React - Commit 02

--- Trilha_React/nlw-expert-notes/src/app.tsx
+++ Trilha_React/nlw-expert-notes/src/app.tsx
@@ -2,10 +2,16 @@ import logo from './assets/Logo-NLW-Expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+/*=================>Opcional
+const note = {
+	date: new Date(),
+	content: "Podemos utilizar o pacote chalk para exibir logs no código coloridos para dar um efeito mais legal no terminal. Pdemos trocar tanto a cor do texto quanto a cor de fundo texto impresso no console."
+}
+*/
 export function App() {
 	return (
 		<div className='mx-auto max-w-6xl my-12 space-y-6'>
-			<img src={logo} alt="Logo NLW Expert" />	
+			<img src={logo} alt="Logo NLW Expert" />
 			<form className='w-full'>
 				<input
 					type="text"
@@ -16,9 +22,18 @@ export function App() {
 
 			<div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
 				<NewNoteCard />
-				<NoteCard />
-				<NoteCard />
-				<NoteCard />
+				<NoteCard note={{
+					date: new Date(),
+					content: "O Drizzie possui um plugin do ESLint para evitar que realizaremos updates ou deletes sem where... Para configurar o plugin, é preciso instalar como abaixo: "
+				}} />
+				<NoteCard note={{
+					date: new Date(),
+					content: "No app do NLW vamos criar um layout incrível, assim podemos entregar a melhor experiência para a comunidade. Na aplicação React vamos criar um projeto que permite o usuário salvar notas em texto ou áudio."
+				}} />
+				<NoteCard note={{
+					date: new Date(),
+					content: "Podemos utilizar o pacote chalk para exibir logs no código coloridos para dar um efeito mais legal no terminal. Podemos trocar tanto a cor do texto quanto a cor de fundo texto impresso no console."
+				}} />
 			</div>
 		</div>
 	)
--- Trilha_React/nlw-expert-notes/src/components/note-card.tsx
+++ Trilha_React/nlw-expert-notes/src/components/note-card.tsx
@@ -1,14 +1,49 @@
-export function NoteCard() {
+//import {Root} from '@radix-ui/react-dialog'
+import * as Dialog from '@radix-ui/react-dialog'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
+
+interface NodeCardProps {
+  note: {
+    date: Date
+    content: String
+  }
+}
+
+export function NoteCard({note}: NodeCardProps) {
   return (
-    <button className='rounded-md text-left bg-slate-800 p-5 space-y-6 overflow-hidden outline-none relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
-    <span className='text-sm font-medium text-slate-300'>
-      há 7 dias
-    </span>
-    <p className='text-sm leading-6 text-slate-400'>
-      Podemos utilizar o pacote chalk para exibir logs no código coloridos para dar um efeito mais legal no terminal.
-      Podemos trocar tanto a cor do texto quanto a cor de fundo do texto impresso no console.
-    </p>
-    <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
-  </button>
+    <Dialog.Root>
+      <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden outline-none relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
+      <span className='text-sm font-medium text-slate-300'>
+        {note.date.toISOString()}
+      </span>
+      <p className='text-sm leading-6 text-slate-400'>
+        {note.content}
+      </p>
+      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className='inset-0 fixed bg-black/50' />
+        <Dialog.Content className='fixed overflow-hidden left-1/2 -translate-x-1/2 -translate-y-1/2 top-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
+
+          <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
+            <X className='size-5'/>
+          </Dialog.Close>
+
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className='text-sm font-medium text-slate-300'>
+              {formatDistanceToNow(note.date, {locale: ptBR, addSuffix: true})}
+            </span>
+            <p className='text-sm leading-6 text-slate-400'>
+              {note.content}
+            </p>
+          </div>
+          <button type="button" className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'>
+            Deseja <span className='text-red-400 group-hover:underline'>apagar essa nota</span>?
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>  
   )
 }
