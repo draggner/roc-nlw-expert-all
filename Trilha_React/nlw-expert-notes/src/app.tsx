baf3004caae30be8d9ae3391a49c85d509b3c7ad
Draggner <devdraggnerstudio@gmail.com>
2024-02-24 22:07:53 -0300

    NLW Expert: React - Commit 03

--- Trilha_React/nlw-expert-notes/src/app.tsx
+++ Trilha_React/nlw-expert-notes/src/app.tsx
@@ -1,39 +1,78 @@
+import { ChangeEvent, useState } from 'react'
 import logo from './assets/Logo-NLW-Expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
-/*=================>Opcional
-const note = {
-	date: new Date(),
-	content: "Podemos utilizar o pacote chalk para exibir logs no código coloridos para dar um efeito mais legal no terminal. Pdemos trocar tanto a cor do texto quanto a cor de fundo texto impresso no console."
+interface Note {
+	id: string
+	date: Date
+	content: string
 }
-*/
+
 export function App() {
+	const [search, setSearch] = useState('')
+	const [notes, setNotes] = useState<Note[]>(() => {
+		const notesOnStorage = localStorage.getItem('notes')
+
+		if (notesOnStorage) {
+			return JSON.parse(notesOnStorage)
+		}
+		return []
+	})
+
+	function onNoteCreated(content: string) {
+		const newNote = {
+			id: crypto.randomUUID(),
+			date: new Date(),
+			content,
+		}
+
+		const notesArray = [newNote, ...notes]
+		
+		setNotes(notesArray)
+
+		localStorage.setItem('notes', JSON.stringify(notesArray))
+	}
+
+	function onNoteDeleted(id: string) {
+		const notesArray = notes.filter(note => {
+			return note.id != id
+		})
+
+		setNotes(notesArray)
+		localStorage.setItem('notes', JSON.stringify(notesArray))
+	}
+
+	function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+		const query = event.target.value
+
+		setSearch(query)
+
+	}
+
+	const filterNotes = search !== ''
+		? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+		: notes
+	//console.log(search)
+
 	return (
-		<div className='mx-auto max-w-6xl my-12 space-y-6'>
+		<div className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
 			<img src={logo} alt="Logo NLW Expert" />
 			<form className='w-full'>
 				<input
+					onChange={handleSearch}
 					type="text"
 					placeholder='Busque em suas notas'
 					className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500' />
 			</form>
 			<div className='h-px bg-slate-700'></div>
 
-			<div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
-				<NewNoteCard />
-				<NoteCard note={{
-					date: new Date(),
-					content: "O Drizzie possui um plugin do ESLint para evitar que realizaremos updates ou deletes sem where... Para configurar o plugin, é preciso instalar como abaixo: "
-				}} />
-				<NoteCard note={{
-					date: new Date(),
-					content: "No app do NLW vamos criar um layout incrível, assim podemos entregar a melhor experiência para a comunidade. Na aplicação React vamos criar um projeto que permite o usuário salvar notas em texto ou áudio."
-				}} />
-				<NoteCard note={{
-					date: new Date(),
-					content: "Podemos utilizar o pacote chalk para exibir logs no código coloridos para dar um efeito mais legal no terminal. Podemos trocar tanto a cor do texto quanto a cor de fundo texto impresso no console."
-				}} />
+			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6'>
+				<NewNoteCard onNoteCreated={onNoteCreated} />
+				{filterNotes.map(note => {
+					return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
+				})}
+
 			</div>
 		</div>
 	)
