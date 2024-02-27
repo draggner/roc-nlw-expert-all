476f30fb7644c3116bb7b50f992c2f440a79815f
Draggner <devdraggnerstudio@gmail.com>
2024-02-27 13:25:54 -0300

    NLW Expert: NodeJs - Commit 01

---
+++ Trilha_NodeJs/polls/src/http/server.ts
@@ -0,0 +1,31 @@
+//console.log('Hello World!')
+
+import fastify from 'fastify'
+import { PrismaClient } from "@prisma/client"
+import z from 'zod'
+
+const app = fastify()
+
+const prisma = new PrismaClient()
+
+app.post('/polls', async (request, reply) => {
+  //console.log(request.body)
+
+  const createPollBody = z.object({
+    title: z.string()
+  })
+  
+  const { title } = createPollBody.parse(request.body)
+  
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    }
+  })
+
+  return reply.status(201).send({ pollid: poll.id })
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!')
+})
