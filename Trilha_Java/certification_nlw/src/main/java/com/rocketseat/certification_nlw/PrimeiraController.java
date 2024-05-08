527a8d20790734215d1a74cb6ff2e7913f5e6678
Draggner <devdraggnerstudio@gmail.com>
2024-05-08 14:11:37 -0300

    NLW Expert: Java - Commit 01

---
+++ Trilha_Java/certification_nlw/src/main/java/com/rocketseat/certification_nlw/PrimeiraController.java
@@ -0,0 +1,27 @@
+package com.rocketseat.certification_nlw;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiraController")
+public class PrimeiraController {
+  
+  @GetMapping("/retornarPrimeiraController")
+  public Usuario retornoPrimeiraController() {
+    var usuario = new Usuario("Davi Samuel", 19);
+
+    return usuario;
+  }
+
+  @PostMapping("/meuPrimeiroPost")
+  public String primeiroPost() {
+    return "Meu primeiro POST";
+  }
+
+  record Usuario(String nome, int idade) {
+  }
+
+}
